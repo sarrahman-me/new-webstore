a86ad975ab47c4cf33fc9396e99fe5fa7a0aac29
sarrahman-me <sarrahman.me@gmail.com>
2024-02-10 16:13:13 +0700

    :hammer: menambahkan rekomendasi di wishlist

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -45,24 +45,43 @@ export default function Wishlist() {
 
   return (
     <main>
-      <AppBar arrowBack title="Wishlist" />
-      <div className="p-2 md:p-4 space-y-6 md:space-y-10">
+      <AppBar arrowBack title="Favorit" />
+      <div className="p-2 md:p-4 space-y-6 md:space-y-8">
         <SearchBar />
 
         {!productNull ? (
-          <CatalogProducts
-            title="Produk Favorit"
-            productReplace={productWish}
-            limit={1}
-          />
+          <div className="space-y-6 md:space-y-8">
+            <CatalogProducts
+              title="Produk Favorit"
+              productReplace={productWish}
+              limit={1}
+            />
+            <CatalogProducts
+              atribut={`kategori=${productWish[0].kategori}&ukuran=${productWish[0].ukuran}&motif=${productWish[0].motif}`}
+              title="Mungkin kamu suka"
+              limit={24}
+              pagination={false}
+            />
+            {productWish[1]?.nama_barang &&
+              productWish[0].ukuran !== productWish[1].ukuran &&
+              productWish[0].motif !== productWish[1].motif && (
+                <CatalogProducts
+                  atribut={`kategori=${productWish[1].kategori}&ukuran=${productWish[1].ukuran}&motif=${productWish[1].motif}`}
+                  title="Rekomendasi Lain"
+                  limit={24}
+                  pagination={false}
+                />
+              )}
+          </div>
         ) : (
-          <HelperComponent
-            imageUrl="https://tokokeramik-assets.s3.ap-southeast-1.amazonaws.com/9169253-removebg-preview.png"
-            title="Tidak ada yang favorit"
-          />
+          <div>
+            <HelperComponent
+              imageUrl="https://tokokeramik-assets.s3.ap-southeast-1.amazonaws.com/9169253-removebg-preview.png"
+              title="Tidak ada yang favorit"
+            />
+            <CatalogProducts title="Mungkin kamu suka" />
+          </div>
         )}
-
-        <CatalogProducts title="Mungkin kamu suka" />
       </div>
       <div className="text-center text-xs text-slate-500 py-10">
         <p>Disupport oleh Toko Keramik Indonesia</p>
